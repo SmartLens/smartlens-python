4a790a0dbf09647d5fb11cc3888e6e3be16f70c7
Michael Royzen <michael@smartlens.ai>
2020-09-10 22:44:45 -0700

    0.9.1 release

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="smartlens-smartlens", # Replace with your own username
-    version="0.9.0",
+    name="smartlens", # Replace with your own username
+    version="0.9.1",
     author="SmartLens Technologies, Inc.",
     author_email="michael@smartlens.ai",
     description="A Python library for the SmartLens API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/SmartLens/smartlens-python/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
